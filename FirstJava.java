c19bb14b688cf0b085cde11100503ef5a8cde364
Josiah-Hernandez <jhern88@me.com>
2022-08-02 10:32:39 -0700

    FirstJava

---
+++ FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava{
+    public static void main (String[]args){
+        System.out.println ("My Name Is Coding Dojo"
+        + "\n I Am 100 Years Old"
+        + "\n My Hometown Is Burbank, CA");
+    }
+}
